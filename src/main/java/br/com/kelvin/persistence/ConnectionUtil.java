5680c48cc5a4a5435dc252c9b761b4476760ec64
kelvin-de-oliveira <kelvin.oliveira.0110@gmail.com>
2025-08-24 00:28:21 -0300

    feat: implementando conexão com o banco

---
+++ src/main/java/br/com/kelvin/persistence/ConnectionUtil.java
@@ -0,0 +1,17 @@
+package br.com.kelvin.persistence;
+
+import lombok.NoArgsConstructor;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@NoArgsConstructor(access = PRIVATE)
+public class ConnectionUtil {
+    public static Connection getConnection() throws SQLException{
+        return DriverManager.getConnection("jdbc:mysql://localhost/jdbc_fundamentals", "root", "123456789");
+    }
+}
+
